cec90f9976fe250f19aeca22fab6d7843e3a0aa3
Vaibhav Kaushik <vaibhavsingh5373@gmail.com>
2024-08-02 17:30:50 +0530

    heyyy

---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/pin");
+
+const userSchema = mongoose.Schema({
+   usename: String,
+   password: String,
+   email: String,
+   profileImage:String,
+   DateofBirth: Date,
+   borads:{
+    type:Array,
+    default:[]
+   },
+   posts:[{
+      type: mongoose.Schema.Types.ObjectId,
+        ref: "post"
+   }]
+});
+
+userSchema.plugin(plm);
+
+module.exports=mongoose.model("user",userSchema)
